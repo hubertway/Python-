f645921866cceedfcab1fc2ef4b80df2a0b684f6
hubertway <32203359+hubertway@users.noreply.github.com>
2022-05-29 21:21:22 +0800

    len(s)-1-i

--- 第三章 使用字符串、列表和元组/3-2 输出字母在字符串中位置索引.py
+++ 第三章 使用字符串、列表和元组/3-2 输出字母在字符串中位置索引.py
@@ -1,7 +1,7 @@
 s = input()
 a,b=input().split(" ")
 for i in range(len(s)):
-    if s[len(s) - 1 - i] == a:
+    if s[len(s) - 1 - i]==a:
         print(len(s) - 1 - i , a)
-    elif s[len(s) - 1 - i] == b:
+    elif s[len(s) - 1 - i]==b:
         print(len(s) - 1 - i , b)
