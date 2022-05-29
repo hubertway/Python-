fca63daca01a28182726cf1e6531620a58a7270a
hubertway <32203359+hubertway@users.noreply.github.com>
2022-05-29 21:02:28 +0800

    Update 3-1 大于身高的平均值.py

--- 第三章 使用字符串、列表和元组/3-1 大于身高的平均值.py
+++ 第三章 使用字符串、列表和元组/3-1 大于身高的平均值.py
@@ -1,1 +1,8 @@
-
+l = input().split(" ")
+sum = 0
+for i in range(len(l)):
+    sum = sum + int(l[i])
+avg = sum / len(l)
+for i in range(len(l)):
+    if int(l[i]) > avg:
+        print(l[i],end=" ")
