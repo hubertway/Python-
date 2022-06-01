fff3c55d9e097f97bb4b8a3ba2b2fd0bdbc42f89
hubertway <32203359+hubertway@users.noreply.github.com>
2022-06-01 22:08:25 +0800

    虽然题目没说，但是还是比较i+

---
+++ 第四章 条件、循环和其他语句/4-10 求π的近似值.py
@@ -0,0 +1,11 @@
+er = float(input())
+m1 = 1
+m2 = 1+1/4
+s = 3
+while True:
+    if abs(m2-m1) <= er:
+        print("{:.6f}".format((6*m1)**0.5))
+        break
+    m1 = m2
+    m2 += 1/s**2
+    s += 1
